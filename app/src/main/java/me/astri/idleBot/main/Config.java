7eae16eef949e90b70d52b762c4047e56b1a5e2b
Astri_ <74017420+Astri2@users.noreply.github.com>
2021-09-03 00:41:48 +0200

    Remove .env lib

--- app/src/main/java/me/astri/idleBot/main/Config.java
+++ app/src/main/java/me/astri/idleBot/main/Config.java
@@ -1,16 +1,7 @@
 package me.astri.idleBot.main;
 
-import io.github.cdimascio.dotenv.Dotenv;
-
 public class Config {
-    private static final Dotenv dotenv = Dotenv.load();
-
-    //public static String get(String key) {
-    //    return dotenv.get(key.toUpperCase());
-    //}
-
     public static String get(String key) {
         return System.getenv().get(key.toUpperCase());
     }
-
 }
