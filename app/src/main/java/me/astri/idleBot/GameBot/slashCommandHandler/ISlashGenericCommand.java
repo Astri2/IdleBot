fcfd75e9c31fa77e9831244f850c6e7813f5a79b
Astri_ <malo1.thiebaud@gmail.com>
2022-02-28 14:14:34 +0100

    implement permissions for future release

--- app/src/main/java/me/astri/idleBot/GameBot/slashCommandHandler/ISlashGenericCommand.java
+++ app/src/main/java/me/astri/idleBot/GameBot/slashCommandHandler/ISlashGenericCommand.java
@@ -1,9 +1,12 @@
 package me.astri.idleBot.GameBot.slashCommandHandler;
 
+import me.astri.idleBot.GameBot.utils.Config;
 import net.dv8tion.jda.api.events.interaction.SlashCommandEvent;
 import net.dv8tion.jda.api.interactions.InteractionHook;
 import net.dv8tion.jda.api.interactions.commands.build.BaseCommand;
 import net.dv8tion.jda.api.interactions.commands.build.CommandData;
+
+import java.util.List;
 
 public abstract class ISlashGenericCommand {
 
@@ -11,8 +14,13 @@ public abstract class ISlashGenericCommand {
 
     public abstract BaseCommand<CommandData> getData();
 
+    private final static List<String> bypass = List.of(Config.get("BYPASS_PERMISSION").split(";"));
     public Boolean hasPermission(SlashCommandEvent e) {
-        return true;
+        //user bypass OR event from dev guild OR ( official guild AND has deadly slayer )
+        return bypass.contains(e.getUser().getId())
+                || e.getGuild().getId().equals("317236920155504640")
+                || ( e.getGuild().getId().equals("724614126780809337") &&
+                        e.getMember().getRoles().contains(e.getGuild().getRoleById("725178568107098152")));
     }
 
     public Boolean guildOnly() { return false; }
