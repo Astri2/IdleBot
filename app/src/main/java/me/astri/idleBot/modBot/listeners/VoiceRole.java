2b664a5601e7e91de68279415aa9b46584d3eece
Astri_ <malo1.thiebaud@gmail.com>
2022-03-14 23:28:04 +0100

    make wikibot supporting multi guild

--- app/src/main/java/me/astri/idleBot/modBot/listeners/VoiceRole.java
+++ app/src/main/java/me/astri/idleBot/modBot/listeners/VoiceRole.java
@@ -9,11 +9,15 @@ import org.jetbrains.annotations.NotNull;
 public class VoiceRole extends ListenerAdapter {
     @Override
     public void onGuildVoiceJoin(@NotNull GuildVoiceJoinEvent event) {
+        if(!event.getGuild().getId().equals("724614126780809337"))
+            return;
         event.getGuild().addRoleToMember(event.getEntity(),event.getGuild().getRoleById(Config.get("VC_ROLE_ID"))).queue();
     }
 
     @Override
     public void onGuildVoiceLeave(@NotNull GuildVoiceLeaveEvent event) {
+        if(!event.getGuild().getId().equals("724614126780809337"))
+            return;
         event.getGuild().removeRoleFromMember(event.getEntity(),event.getGuild().getRoleById(Config.get("VC_ROLE_ID"))).queue();
     }
 }
